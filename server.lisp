8dfa909a0367170e072872d931a4a33641fed1cd
Laurent Cimon <laurent@nilio.ca>
2025-04-27 18:48:02 -0400

    Add favicon to html

--- server.lisp
+++ server.lisp
@@ -48,6 +48,7 @@
 	 (:head
 	  (:meta :name "viewport" :content "width=device-width,initial-scale=1")
 	  (:link :rel "stylesheet" :href "style.css")
+	  (:link :rel "icon" :type "image/x-icon" :href "/favicon.ico")
 	  (:title (str (if ,title (concatenate 'string header ,title) header))))
 	 (:body
 	  (:main
